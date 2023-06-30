92ffb5d7c6c77dd2069a7a938dd1cddff27cd2c1
Evgenii <evgkhm@mail.ru>
2023-06-30 11:23:50 +0300

    101 done

--- 101_symmetric_tree/101_symmetric_tree.go
+++ 101_symmetric_tree/101_symmetric_tree.go
@@ -9,13 +9,25 @@ type TreeNode struct {
 }
 
 func main() {
-	node4 := TreeNode{
+	//TEST CASE1
+	/*node4R := TreeNode{
 		Val:   4,
 		Left:  nil,
 		Right: nil,
 	}
 
-	node3 := TreeNode{
+	node4L := TreeNode{
+		Val:   4,
+		Left:  nil,
+		Right: nil,
+	}
+
+	node3R := TreeNode{
+		Val:   3,
+		Left:  nil,
+		Right: nil,
+	}
+	node3L := TreeNode{
 		Val:   3,
 		Left:  nil,
 		Right: nil,
@@ -23,22 +35,54 @@ func main() {
 
 	node2 := TreeNode{
 		Val:   2,
-		Left:  &node4,
-		Right: &node3,
+		Left:  &node4L,
+		Right: &node3R,
 	}
 
 	node1 := TreeNode{
 		Val:   2,
-		Left:  &node3,
-		Right: &node4,
+		Left:  &node3L,
+		Right: &node4R,
 	}
 
 	node := TreeNode{
 		Val:   1,
 		Left:  &node1,
 		Right: &node2,
+	}*/
+
+	//TEST CASE2
+	/*node3RL := TreeNode{
+		Val:   3,
+		Left:  nil,
+		Right: nil,
 	}
 
+	node3RR := TreeNode{
+		Val:   3,
+		Left:  nil,
+		Right: nil,
+	}
+
+	node2R := TreeNode{
+		Val:   2,
+		Left:  nil,
+		Right: &node3RR,
+	}
+
+	node2L := TreeNode{
+		Val:   2,
+		Left:  nil,
+		Right: &node3RL,
+	}
+
+	node := TreeNode{
+		Val:   1,
+		Left:  &node2L,
+		Right: &node2R,
+	}*/
+
+	//TEST CASE3
 	/*node2 := TreeNode{
 		Val:   3,
 		Left:  nil,
@@ -48,43 +92,69 @@ func main() {
 	node1 := TreeNode{
 		Val:   2,
 		Left:  nil,
-		Right: &node2,
+		Right: nil,
 	}
 
 	node := TreeNode{
 		Val:   1,
 		Left:  &node1,
-		Right: &node1,
+		Right: &node2,
 	}*/
 
-	isSymmetric(&node)
+	//TEST CASE4
+	node6 := TreeNode{
+		Val:   5,
+		Left:  nil,
+		Right: nil,
+	}
+
+	node5 := TreeNode{
+		Val:   5,
+		Left:  nil,
+		Right: nil,
+	}
+	node4 := TreeNode{
+		Val:   4,
+		Left:  nil,
+		Right: nil,
+	}
+
+	node2 := TreeNode{
+		Val:   3,
+		Left:  &node4,
+		Right: &node5,
+	}
+
+	node3 := TreeNode{
+		Val:   3,
+		Left:  &node6,
+		Right: nil,
+	}
+
+	node1 := TreeNode{
+		Val:   2,
+		Left:  &node2,
+		Right: &node3,
+	}
+
+	isSymmetric(&node1)
 
 }
 
 func isSymmetricHelper(res *bool, rootLeft *TreeNode, rootRight *TreeNode) bool {
-	if rootLeft == nil || rootRight == nil {
+	if rootLeft == nil && rootRight == nil {
 		return *res
 	}
 
-	if rootLeft.Val == rootRight.Val &&
-		rootLeft.Left != nil && rootLeft.Right != nil &&
-		rootRight.Left != nil && rootRight.Right != nil {
-		*res = true
-		isSymmetricHelper(res, rootLeft.Left, rootRight.Left)
-		isSymmetricHelper(res, rootLeft.Right, rootRight.Right)
-	} else if rootLeft.Val != rootRight.Val {
+	if rootLeft == nil || rootRight == nil {
 		*res = false
 		return *res
-	} else if rootLeft.Left == nil && rootLeft.Right != nil {
-		*res = false
-		return *res
-	} else if rootLeft.Left != nil && rootLeft.Right == nil {
-		*res = false
-		return *res
-	} else if rootRight.Left == nil && rootRight.Right != nil {
-		*res = false
-		return *res
-	} else if rootRight.Left != nil && rootRight.Right == nil {
+	}
+
+	if rootLeft.Val == rootRight.Val {
+		isSymmetricHelper(res, rootLeft.Left, rootRight.Right)
+		isSymmetricHelper(res, rootLeft.Right, rootRight.Left)
+	} else {
 		*res = false
 		return *res
 	}
