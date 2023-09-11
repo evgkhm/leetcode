9513f0147c857a401b02b196f7bd57f66f3ba171
Evgenii <evgkhm@mail.ru>
2023-09-11 20:29:50 +0300

    1979

---
+++ easy/1979._Find_Greatest_Common_Divisor_of_Array/1979._Find_Greatest_Common_Divisor_of_Array.go
@@ -0,0 +1,25 @@
+package _979__Find_Greatest_Common_Divisor_of_Array
+
+func findGCD(nums []int) int {
+	min, max := 1000, 1
+	for _, val := range nums {
+		if val < min {
+			min = val
+		}
+		if val > max {
+			max = val
+		}
+	}
+	return nod(min, max)
+}
+
+func nod(a int, b int) int {
+	for a != 0 && b != 0 {
+		if a > b {
+			a = a % b
+		} else {
+			b = b % a
+		}
+	}
+	return a + b
+}
