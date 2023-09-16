f4cfa9300741709d2a7e172675b2b7f3c1b19ad6
Evgenii <evgkhm@mail.ru>
2023-09-16 11:19:28 +0300

    2095

---
+++ medium/2095._Delete_the_Middle_Node_of_a_Linked_List/2095._Delete_the_Middle_Node_of_a_Linked_List.go
@@ -0,0 +1,48 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main() {
+	//node6 := ListNode{Val: 6, Next: nil}
+	//node5 := ListNode{Val: 2, Next: &node6}
+	//node4 := ListNode{Val: 1, Next: &node5}
+	//node3 := ListNode{Val: 7, Next: &node4}
+	//node2 := ListNode{Val: 4, Next: &node3}
+	//node1 := ListNode{Val: 3, Next: &node2}
+	head := ListNode{Val: 1, Next: nil}
+
+	deleteMiddle(&head)
+}
+
+func deleteMiddle(head *ListNode) *ListNode {
+	length := 0
+	findLength(head, &length)
+	if length == 1 {
+		return nil
+	}
+	middle := (length / 2) - 1
+
+	res := head
+
+	for i := 0; i <= middle; i++ {
+		if i == middle {
+			res.Next = res.Next.Next
+		} else {
+			res = res.Next
+		}
+	}
+
+	return head
+}
+
+func findLength(head *ListNode, length *int) {
+	if head == nil {
+		return
+	}
+	*length++
+	findLength(head.Next, length)
+	return
+}
