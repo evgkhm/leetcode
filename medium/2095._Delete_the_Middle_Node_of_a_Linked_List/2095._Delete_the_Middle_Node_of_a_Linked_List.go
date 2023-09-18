fed4301e3f0842ffb96463a0988c55acfdf2644d
Evgenii <evgkhm@mail.ru>
2023-09-18 09:12:57 +0300

    2095

--- medium/2095._Delete_the_Middle_Node_of_a_Linked_List/2095._Delete_the_Middle_Node_of_a_Linked_List.go
+++ medium/2095._Delete_the_Middle_Node_of_a_Linked_List/2095._Delete_the_Middle_Node_of_a_Linked_List.go
@@ -6,43 +6,62 @@ type ListNode struct {
 }
 
 func main() {
-	//node6 := ListNode{Val: 6, Next: nil}
-	//node5 := ListNode{Val: 2, Next: &node6}
-	//node4 := ListNode{Val: 1, Next: &node5}
-	//node3 := ListNode{Val: 7, Next: &node4}
-	//node2 := ListNode{Val: 4, Next: &node3}
-	//node1 := ListNode{Val: 3, Next: &node2}
-	head := ListNode{Val: 1, Next: nil}
+	node6 := ListNode{Val: 6, Next: nil}
+	node5 := ListNode{Val: 2, Next: &node6}
+	node4 := ListNode{Val: 1, Next: &node5}
+	node3 := ListNode{Val: 7, Next: &node4}
+	node2 := ListNode{Val: 4, Next: &node3}
+	node1 := ListNode{Val: 3, Next: &node2}
+	head := ListNode{Val: 1, Next: &node1}
 
 	deleteMiddle(&head)
 }
 
 func deleteMiddle(head *ListNode) *ListNode {
-	length := 0
-	findLength(head, &length)
-	if length == 1 {
+	if head.Next == nil {
 		return nil
 	}
-	middle := (length / 2) - 1
 
-	res := head
+	slow, fast := head, head
+	prevSlow := &ListNode{}
 
-	for i := 0; i <= middle; i++ {
-		if i == middle {
-			res.Next = res.Next.Next
-		} else {
-			res = res.Next
-		}
+	for fast.Next != nil {
+		prevSlow = slow
+		slow = slow.Next
+		fast = fast.Next.Next
 	}
+
+	prevSlow.Next = prevSlow.Next.Next
 
 	return head
 }
 
-func findLength(head *ListNode, length *int) {
-	if head == nil {
-		return
-	}
-	*length++
-	findLength(head.Next, length)
-	return
-}
+//func deleteMiddle(head *ListNode) *ListNode {
+//	length := 0
+//	findLength(head, &length)
+//	if length == 1 {
+//		return nil
+//	}
+//	middle := (length / 2) - 1
+//
+//	res := head
+//
+//	for i := 0; i <= middle; i++ {
+//		if i == middle {
+//			res.Next = res.Next.Next
+//		} else {
+//			res = res.Next
+//		}
+//	}
+//
+//	return head
+//}
+//
+//func findLength(head *ListNode, length *int) {
+//	if head == nil {
+//		return
+//	}
+//	*length++
+//	findLength(head.Next, length)
+//	return
+//}
